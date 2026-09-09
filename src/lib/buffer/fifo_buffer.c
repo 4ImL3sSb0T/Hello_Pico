5724fb683b9ea9e3afd63dc59ec3da6b92e5b368
4ImL3sSb0T <wansheng06@outlook.com>
2026-09-09 12:55:21 +0800

    添加 FIFO 缓冲区的互斥锁支持，更新相关函数以增强线程安全性

--- src/lib/buffer/fifo_buffer.c
+++ src/lib/buffer/fifo_buffer.c
@@ -1,6 +1,40 @@
 #include "fifo_buffer.h"
 #include <string.h>
 
+static inline int fifo_buffer_is_full(fifo_buffer_t *fifo) {
+    return ((fifo->head + 1) % fifo->size) == fifo->tail;
+}
+
+static inline int fifo_buffer_is_empty(fifo_buffer_t *fifo) {
+    return fifo->head == fifo->tail;
+}
+
+#ifdef FIFO_BUFFER_USING_MUTEX
+static inline void fifo_buffer_lock(fifo_buffer_t *fifo) {
+    if (fifo->lock) fifo->lock(fifo->mutex)
+}
+
+static inline void fifo_buffer_unlock(fifo_buffer_t *fifo) {
+    if (fifo->unlock) fifo->unlock(fifo->mutex);
+}
+#endif
+
+#ifdef FIFO_BUFFER_USING_MUTEX
+int fifo_buffer_init(fifo_buffer_t *fifo, uint8_t *buffer, size_t size, void *mutex, fifo_buffer_mutex_lock_t lock, fifo_buffer_mutex_unlock_t unlock)
+{
+    if (!fifo || !buffer || size == 0) return -1; // Invalid parameters
+
+    fifo->buffer = buffer;
+    fifo->size = size;
+    fifo->head = 0;
+    fifo->tail = 0;
+    fifo->mutex = mutex;
+    fifo->lock = lock;
+    fifo->unlock = unlock;
+
+    return 0;
+}
+#else
 int fifo_buffer_init(fifo_buffer_t *fifo, uint8_t *buffer, size_t size)
 {
     if (!fifo || !buffer || size == 0) return -1; // Invalid parameters
@@ -12,17 +46,41 @@ int fifo_buffer_init(fifo_buffer_t *fifo, uint8_t *buffer, size_t size)
 
     return 0;
 }
+#endif
 
 int fifo_buffer_write(fifo_buffer_t *fifo, const uint8_t *data, size_t length) {
     if (!fifo || !data || length == 0) return -1; // Invalid parameters
 
+#ifdef FIFO_BUFFER_USING_MUTEX
+    fifo_buffer_lock(fifo);
+#endif
+
+    int left = fifo_buffer_get_left(fifo);
+    if (left < 0 || (size_t)left < length) return -1; // Not enough space
+
+    for (size_t i = 0; i < length; ++i) {
+        fifo->buffer[fifo->head] = data[i];
+        fifo->head = (fifo->head + 1) % fifo->size;
+    }
     
+#ifdef FIFO_BUFFER_USING_MUTEX
+    fifo_buffer_unlock(fifo);
+#endif
+
+    return 0;
 }
 
 int fifo_buffer_read(fifo_buffer_t *fifo, uint8_t *data, size_t length);
 
 int fifo_buffer_peek(fifo_buffer_t *fifo, uint8_t *data, size_t length, size_t offset);
 
-int fifo_buffer_get_left(fifo_buffer_t *fifo);
+int fifo_buffer_get_left(fifo_buffer_t *fifo) {
+    if (!fifo) return -1; // Invalid parameter
+    return (fifo->size - 1) - fifo_buffer_get_used(fifo);
+    
+}
 
-int fifo_buffer_get_used(fifo_buffer_t *fifo);
+int fifo_buffer_get_used(fifo_buffer_t *fifo) {
+    if (!fifo) return -1; // Invalid parameter
+    return (fifo->head + fifo->size - fifo->tail) % fifo->size;
+}
--- src/lib/buffer/fifo_buffer.h
+++ src/lib/buffer/fifo_buffer.h
@@ -1,20 +1,34 @@
 #include <stdint.h>
 #include <stddef.h>
 
+#ifdef FIFO_BUFFER_USING_MUTEX
+typedef void (*fifo_buffer_mutex_lock_t)(void *mutex);
+typedef void (*fifo_buffer_mutex_unlock_t)(void *mutex);
+#endif
+
 typedef struct {
     uint8_t *buffer;
     size_t size;
     size_t head;
     size_t tail;
+#ifdef FIFO_BUFFER_USING_MUTEX
+    void *mutex;
+    fifo_buffer_mutex_lock_t lock;
+    fifo_buffer_mutex_unlock_t unlock;
+#endif
 } fifo_buffer_t;
 
+#ifdef FIFO_BUFFER_USING_MUTEX
+int fifo_buffer_init(fifo_buffer_t *fifo, uint8_t *buffer, size_t size, void *mutex, fifo_buffer_mutex_lock_t lock, fifo_buffer_mutex_unlock_t unlock);
+#else
 int fifo_buffer_init(fifo_buffer_t *fifo, uint8_t *buffer, size_t size);
+#endif
 
 int fifo_buffer_write(fifo_buffer_t *fifo, const uint8_t *data, size_t length);
 
 int fifo_buffer_read(fifo_buffer_t *fifo, uint8_t *data, size_t length);
 
-int fifo_buffer_peek(fifo_buffer_t *fifo, uint8_t *data, size_t length);
+int fifo_buffer_peek(fifo_buffer_t *fifo, uint8_t *data, size_t length, size_t offset);
 
 int fifo_buffer_get_left(fifo_buffer_t *fifo);
 
